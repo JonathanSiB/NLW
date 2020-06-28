0e6dae707a61dc3268d7b622cf7b03ddf05c9558
JonathanSiB <jonathansilb@hotmail.com>
2020-06-28 03:51:58 -0300

    ok

---
+++ scripts/intro.js
@@ -0,0 +1,69 @@
+// comments
+
+// variaveis,tipos de dados
+// var ou let myvar = "hello"
+// const myconst ="hello"
+
+// document.write(myconst+myvar)
+// string
+// "isso string"
+// 'isso string'
+// `isso e string´
+
+// number
+// // // const n1 = 1
+// // const n2 = 12
+// document.write(n1+n2)
+
+// boolean true ou false
+// // // const btrue = true
+// // const bfalse = false
+// document.write(bfalse)
+
+///objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura:"1,80m",
+//   idade:24,
+//   solteiro:true,
+//   correr(){
+//   document.write("executar uma grande logica de correr")
+// }
+// }
+
+// pessoa.correr()
+
+//Array - vetores
+// const colecaoDeBolinhas = ["blue","green",1,{name:"Myname"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+//função - atalhos - funcionalidades -reuso de código
+
+//registrar função
+// function sayMyName(name){
+  
+//   document.write(name)
+// }
+
+// //executar
+// sayMyName("Douglas")
+// sayMyName("teodoro")
+// sayMyName("Bryan")
+
+//condicionais
+// const notaFinal = 5
+
+// if (notaFinal < 5){
+//   //caminho 1
+//   document.write("Reprovado")
+// }
+// else {
+//   //caminho 2
+//     document.write("Aprovado")
+// }
+
+//loop repetições
+ // for (i = 0; i < 10; i++){
+ //   document.write(`<p>${i}</p>`)
+ // }
+
